40b23f7213cdc19e43643084121fb6d95ed39b23
Adam Sawicki <sawickiap@poczta.onet.pl>
2025-12-24 23:29:05 +0100

    Added copyright header comment to all source files

--- src/core.cpp
+++ src/core.cpp
@@ -1,3 +1,11 @@
+// Copyright (c) 2025 Adam Sawicki
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, subject to the terms of the MIT License.
+//
+// See the LICENSE file in the project root for full license text.
+
 #include <jd3d12/core.hpp>
 #include <jd3d12/config.hpp>
 #include "internal_utils.hpp"
--- tests/shaders/CopySquaredTyped.hlsl
+++ tests/shaders/CopySquaredTyped.hlsl
@@ -1,3 +1,11 @@
+// Copyright (c) 2025 Adam Sawicki
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, subject to the terms of the MIT License.
+//
+// See the LICENSE file in the project root for full license text.
+
 Buffer<float> src_buf : register(t0);
 RWBuffer<float> dst_buf : register(u0);
 
