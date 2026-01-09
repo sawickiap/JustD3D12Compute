da967079bb71d5d6483d492636417d0c2283ed9c
sawickiap <reg@plastic-demo.pl>
2026-01-09 21:21:06 +0100

    Updated year to 2026 (files I forgot to add)

--- tests/shaders/include_test.hlsl
+++ tests/shaders/include_test.hlsl
@@ -1,3 +1,14 @@
+// Copyright (c) 2025-2026 Adam Sawicki
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, subject to the terms of the MIT License.
+//
+// See the LICENSE file in the project root for full license text.
+
+// This file is deliberately saved in UTF-16 LE, unlike other .hlsl files, to test
+// character encoding.
+
 #include "include_test_header.hlsl"
 
 [numthreads(1, 1, 1)]
--- tests/shaders/include_test_header.hlsl
+++ tests/shaders/include_test_header.hlsl
@@ -1,1 +1,12 @@
+// Copyright (c) 2025-2026 Adam Sawicki
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, subject to the terms of the MIT License.
+//
+// See the LICENSE file in the project root for full license text.
+
+// This file is deliberately saved in UTF-16 LE, unlike other .hlsl files, to test
+// character encoding.
+
 RWBuffer<float> typed_buf : register(u0);
